efc3a5bb73ec4cf16696cbfe38da613a2b0ae89c
fishKoKo <49087865+fishKoKo@users.noreply.github.com>
2019-04-18 17:52:15 +0800

    Add files via upload

---
+++ TestString4_6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TestString4_6 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+        System.out.println(str.endsWith("abc"));
+	}
+
+}
