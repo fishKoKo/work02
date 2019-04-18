2dd8a56ac94b02b34606dfd6aa1cdaa155b308e8
fishKoKo <49087865+fishKoKo@users.noreply.github.com>
2019-04-18 17:30:57 +0800

    Add files via upload

--- TestString4_2.java
+++ TestString4_2.java
@@ -5,10 +5,9 @@ public class TestString4_2 {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		String bString="asdfesdafe222.111.222.111asdwdf111.111.222.222saw";
-		String regEx="((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)";
-		Pattern p = Pattern.compile(regEx);
-		Matcher m = p.matcher(bString);
+		String str="asdfesdafe222.111.222.111asdwdf111.111.222.222saw";
+		Pattern p = Pattern.compile("((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)");
+		Matcher m = p.matcher(str);
 
 		while (m.find()) {
 		String result=m.group();
